48456e4df909aaeb4bd467977ed4d2a016de2197
Marcilio <marciliopi@hotmail.com>
2025-11-09 11:37:57 -0300

    atualiza

---
+++ aula1_sqlite.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+banco = sqlite3.connect('primeiro_banco.db')
+
+cursor = banco.cursor()
+cursor.execute("CREATE TABLE pessoas (nome text, idade integer, email text)")
+cursor.execute("INSERT INTO pessoas VALUES('Maria', 40,'maria_123@gmail.com')")
+cursor.execute("INSERT INTO pessoas VALUES('Joao', 38,'joao_123@gmail.com')")
+cursor.execute("INSERT INTO pessoas VALUES('jose', 45,'jose_123@gmail.com')")
+banco.commit()
