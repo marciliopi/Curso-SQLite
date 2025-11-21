0176582282542992802a1bdb1fa3fc75c3f333c7
Marcilio <marciliopi@hotmail.com>
2025-11-21 13:50:28 -0300

    subir
    hhh

--- aula1_sqlite.py
+++ aula1_sqlite.py
@@ -3,13 +3,16 @@ import sqlite3
 banco = sqlite3.connect('primeiro_banco.db')
 
 cursor = banco.cursor()
-cursor.execute("CREATE TABLE pessoas (nome text, idade integer, email text)")
+
+""" cursor.execute("CREATE TABLE pessoas (nome text, idade integer, email text)")
 
 cursor.execute("INSERT INTO pessoas VALUES('Maria', 40,'maria_123@gmail.com')")
 cursor.execute("INSERT INTO pessoas VALUES('Joao', 38,'joao_123@gmail.com')")
 cursor.execute("INSERT INTO pessoas VALUES('jose', 45,'jose_123@gmail.com')")
+ """
+cursor.execute("INSERT INTO pessoas VALUES('Tony', 30,'tony@gmail.com')")
 
 banco.commit()
 
-cursor.execute("SELECT * FROM pessoas")
-print(cursor.fetchall())
+""" cursor.execute("SELECT * FROM pessoas")
+print(cursor.fetchall()) """
