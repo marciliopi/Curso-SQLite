152c164e8f5c2e8c1bc62e2fafbc0e460a3901be
Marcilio <marciliopi@hotmail.com>
2025-11-21 11:41:09 -0300

    subir
    dzzafasf

--- aula1_sqlite.py
+++ aula1_sqlite.py
@@ -4,7 +4,12 @@ banco = sqlite3.connect('primeiro_banco.db')
 
 cursor = banco.cursor()
 cursor.execute("CREATE TABLE pessoas (nome text, idade integer, email text)")
+
 cursor.execute("INSERT INTO pessoas VALUES('Maria', 40,'maria_123@gmail.com')")
 cursor.execute("INSERT INTO pessoas VALUES('Joao', 38,'joao_123@gmail.com')")
 cursor.execute("INSERT INTO pessoas VALUES('jose', 45,'jose_123@gmail.com')")
+
 banco.commit()
+
+cursor.execute("SELECT * FROM pessoas")
+print(cursor.fetchall())
